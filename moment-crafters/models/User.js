96dbfea3716d9bb9ec7541e4a1fdf614dba69fbf
shreyas <157875953+shreyas2228@users.noreply.github.com>
2025-03-31 18:38:00 +0530

    Add moment-crafters/models/User.js 1743426482040

---
+++ moment-crafters/models/User.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const { isEmail } = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please enter your name'],
+    trim: true
+  },
+  email: {
+    type: String,
+    required: [true, 'Please enter your email'],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: [8, 'Minimum password length is 8 characters'],
+    select: false
+  },
+  role: {
+    type: String,
+    enum: ['user', 'vendor', 'admin'],
+    default: 'user'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
